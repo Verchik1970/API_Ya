8ba1717eec00a0b2110f0549e414d06d0c6596b9
Verchik1970 <Verchik1970@gmail.com>
2023-08-09 12:21:25 +0300

    broken

---
+++ src/test/java/TestPostman.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+public class TestPostman {
+    @Test
+
+    public void postmanTestGet(){
+        RestAssured.when().get("https://postman-echo.com/get").
+        then().assertThat().statusCode(200);
+        }
+}
+
