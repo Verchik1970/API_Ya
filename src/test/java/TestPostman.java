4d22f2d462846c4428d57e0efaf59a7cff5c40ac
Verchik1970 <Verchik1970@gmail.com>
2023-08-16 14:57:50 +0300

    Post test

--- src/test/java/TestPostman.java
+++ src/test/java/TestPostman.java
@@ -1,12 +1,24 @@
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestPostman {
     @Test
+    @DisplayName("проверка кода 200")
 
-    public void postmanTestGet(){
-        RestAssured.when().get("https://postman-echo.com/get").
-        then().assertThat().statusCode(200);
-        }
+    public void postmanTestGet() {
+        RestAssured.when().get("https://postman-echo.com/get?foo1=bar1&foo2=bar2").
+                then().assertThat().statusCode(200);
+
+    }
+
+    @Test
+    @DisplayName("Проверка POST")
+    public void postmanTestPost() {
+        RestAssured.when().post("https://api-101.glitch.me/customers").
+                then().assertThat().body("userId", Matchers.is("userId"));
+    }
+
 }
 
