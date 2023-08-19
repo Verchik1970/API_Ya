df3b97cc845fd797e586127cbbb32c34b193d9f7
Verchik1970 <Verchik1970@gmail.com>
2023-08-19 17:05:27 +0300

    Api key received

--- src/test/java/TestPostman.java
+++ src/test/java/TestPostman.java
@@ -1,24 +1,42 @@
 import io.restassured.RestAssured;
-import org.hamcrest.Matchers;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class TestPostman {
     @Test
     @DisplayName("проверка кода 200")
 
-    public void postmanTestGet() {
-        RestAssured.when().get("https://postman-echo.com/get?foo1=bar1&foo2=bar2").
-                then().assertThat().statusCode(200);
+    public void skillGetTest() {
+       RestAssured.given().spec(PetsSkill.requestSpecification()).get("/login")
+               .then().statusCode(200);
 
     }
 
     @Test
-    @DisplayName("Проверка POST")
-    public void postmanTestPost() {
-        RestAssured.when().post("https://api-101.glitch.me/customers").
-                then().assertThat().body("userId", Matchers.is("userId"));
+    public void skillApiKeyGetTest() {
+
+        Response response =  given().header("email","ver@mail.ee").header("password", "121970")
+                .when().spec(PetsSkill.requestSpecification()).get("/api/key");
+
+        String getBody = response.getBody().asString();
+
+        System.out.println(getBody);
     }
 
-}
+
+
+
+
+
+    }
+   /* @Test
+    @DisplayName("Проверка POST")
+    public void postmanTestPost() {
+        RestAssured.when().post("https://api-101.glitch.me//customers").
+                then().assertThat().a;
+    }*/
+
 
