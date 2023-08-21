115daad0c7a5b2466f4c60dc314dbb7f1a43b516
Verchik1970 <Verchik1970@gmail.com>
2023-08-21 13:00:39 +0300

    add delete test

--- src/test/java/TestPostman.java
+++ src/test/java/TestPostman.java
@@ -20,12 +20,15 @@ public class TestPostman {
     }
 
     static String authKey;
+    static String email = "ver@mail.ee";
+    static String password = "121970";
+    public String petId;
 
     @BeforeAll
     @DisplayName("Получение авторизационного ключа")
     public static void skillApiKeyGetTest() {
 
-        Response response = given().header("email", "ver@mail.ee").header("password", "121970")
+        Response response = given().header("email", email).header("password", password)
                 .when().spec(PetsSkill.requestSpecification()).get("/api/key");
         String responseStr = response.getBody().asString();
 
@@ -40,22 +43,26 @@ public class TestPostman {
     public void createPetTest() {
         baseURI = "https://petfriends.skillfactory.ru/";
 
-        given()
+        String response = given()
                 .header("auth_key", authKey)
                 .contentType("application/json")
-                .body("{ \"name\": \"Lucky\", \"animal_type\": \"dog\", \"age\": 14 }")
+                .body("{ \"name\": \"Toto\", \"animal_type\": \"dog\", \"age\": 5 }")
                 .when()
                 .post("/api/create_pet_simple")
                 .then()
                 .statusCode(200)
-                .log().body();
+                .extract().response().asString()
+                ;
+        System.out.println(response);
+        JsonPath jsonPath = new JsonPath(response);
+        petId = jsonPath.get("id");
+
     }
 
     @Test
     @DisplayName("Получение списка моих питомцев")
     public void getPetListTest() {
         baseURI = "https://petfriends.skillfactory.ru/";
-
         given()
                 .header("auth_key", authKey)
                 .contentType("application/json")
@@ -68,6 +75,23 @@ public class TestPostman {
 
 
     }
+    @Test
+    @DisplayName("delete")
+    public void deletePetTest() {
+        baseURI = "https://petfriends.skillfactory.ru/";
+
+        given()
+                .header("auth_key", authKey)
+                .contentType("application/json")
+                .when()
+                .delete("/api/pets/"+petId)
+                .then()
+                .statusCode(200)
+                .log().body();
+
+
+    }
+
 }
 
 
